46cb1b72f978de3b2502d2570f4d2bcdad3be7e2
Ben Barrett <barrett777@gmail.com>
2019-11-25 18:26:02 -0800

    Update map

---
+++ playersData.js
@@ -0,0 +1,73 @@
+// NOTE: Please only modify player attributes below such as name and color
+// This file is automatically updated, and if other parts are changed, it will fail to update
+// Names and colors will only be read from the /map/playersData.js file - /update/playersData.js will be overwritten, so don't make changes there
+var playersData = {
+  "players": [
+    {
+      "uuid": "00da2aed-1e63-413b-8dfe-77af59e29863",
+      "name": "00da2aed-1e63-413b-8dfe-77af59e29863",
+      "dimensionId": 0,
+      "position": [
+        1663.3,
+        65.62001,
+        -172.60556
+      ],
+      "color": "DarkGreen"
+    },
+    {
+      "uuid": "2f66b79e-a786-4ec2-8c0d-8c420ff56578",
+      "name": "2f66b79e-a786-4ec2-8c0d-8c420ff56578",
+      "dimensionId": 0,
+      "position": [
+        1488.9489,
+        73.62001,
+        -265.6138
+      ],
+      "color": "DarkRed"
+    },
+    {
+      "uuid": "461c568c-cc24-4fbf-9748-6c501d2dc9f2",
+      "name": "461c568c-cc24-4fbf-9748-6c501d2dc9f2",
+      "dimensionId": 0,
+      "position": [
+        1681.3534,
+        71.62001,
+        -194.58513
+      ],
+      "color": "Purple"
+    },
+    {
+      "uuid": "a62cabac-23ca-49ca-ba0a-3d878af3fc6f",
+      "name": "a62cabac-23ca-49ca-ba0a-3d878af3fc6f",
+      "dimensionId": 0,
+      "position": [
+        1694.5613,
+        71.62001,
+        -213.37317
+      ],
+      "color": "DarkBlue"
+    },
+    {
+      "uuid": "ae9269d5-ad28-4092-be4b-a8db6c7b567e",
+      "name": "ae9269d5-ad28-4092-be4b-a8db6c7b567e",
+      "dimensionId": 0,
+      "position": [
+        1679.9906,
+        75.62001,
+        -212.70615
+      ],
+      "color": "DarkRed"
+    },
+    {
+      "uuid": "f9ffc0a1-24b0-4065-9b89-f92a8864ecbe",
+      "name": "f9ffc0a1-24b0-4065-9b89-f92a8864ecbe",
+      "dimensionId": 0,
+      "position": [
+        1630.0951,
+        65.62001,
+        -130.9854
+      ],
+      "color": "Gold"
+    }
+  ]
+};
