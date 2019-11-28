414252b9cb85a7678f0d303271888f12beb2a92a
Ben Barrett <barrett777@gmail.com>
2019-11-27 20:09:49 -0800

    Update map

--- playersData.js
+++ playersData.js
@@ -6,69 +6,75 @@ var playersData = {
   "players": [
     {
       "uuid": "00da2aed-1e63-413b-8dfe-77af59e29863",
-      "name": "00da2aed-1e63-413b-8dfe-77af59e29863",
+      "name": "?Player 1?",
       "dimensionId": 0,
       "position": [
         1679.6819,
         33.62001,
         -240.3
       ],
-      "color": "DarkGreen"
+      "color": "DarkGreen",
+	  "visible": true
     },
     {
       "uuid": "2f66b79e-a786-4ec2-8c0d-8c420ff56578",
-      "name": "2f66b79e-a786-4ec2-8c0d-8c420ff56578",
+      "name": "?Player 2?",
       "dimensionId": 0,
       "position": [
         1488.9489,
         73.62001,
         -265.6138
       ],
-      "color": "DarkRed"
+      "color": "DarkRed",
+	  "visible": true
     },
     {
       "uuid": "461c568c-cc24-4fbf-9748-6c501d2dc9f2",
-      "name": "461c568c-cc24-4fbf-9748-6c501d2dc9f2",
+      "name": "Jenna",
       "dimensionId": 0,
       "position": [
         1765.5715,
         69.62001,
         -224.706
       ],
-      "color": "Purple"
+      "color": "DeepPink",
+	  "visible": true
     },
     {
       "uuid": "a62cabac-23ca-49ca-ba0a-3d878af3fc6f",
-      "name": "a62cabac-23ca-49ca-ba0a-3d878af3fc6f",
+      "name": "Ben",
       "dimensionId": 1,
       "position": [
         290.53378,
         68.62001,
         -100.66121
       ],
-      "color": "DarkBlue"
+      "color": "DarkGreen",
+	  "visible": true
     },
     {
       "uuid": "ae9269d5-ad28-4092-be4b-a8db6c7b567e",
-      "name": "ae9269d5-ad28-4092-be4b-a8db6c7b567e",
+      "name": "?Player 3?",
       "dimensionId": 0,
       "position": [
         1679.9906,
         75.62001,
         -212.70615
       ],
-      "color": "DarkRed"
+      "color": "DarkRed",
+	  "visible": true
     },
     {
       "uuid": "f9ffc0a1-24b0-4065-9b89-f92a8864ecbe",
-      "name": "f9ffc0a1-24b0-4065-9b89-f92a8864ecbe",
+      "name": "?Player 4?",
       "dimensionId": 0,
       "position": [
         1669.5641,
         69.62001,
         -180.77008
       ],
-      "color": "Gold"
+      "color": "Gold",
+	  "visible": true
     }
   ]
 };
