47af33303187677fd24f8a28bd7293108e1bf6f7
Ben Barrett <barrett777@gmail.com>
2019-12-02 20:17:40 -0800

    Update map

--- playersData.js
+++ playersData.js
@@ -9,72 +9,96 @@ var playersData = {
       "name": "?Player 1?",
       "dimensionId": 0,
       "position": [
-        1686.5012,
-        70.62001,
-        -172.96509
+        1345.3,
+        65.57001,
+        -2064.3
       ],
       "color": "DarkGreen",
-      "visible": true
+      "visible": false
     },
     {
       "uuid": "2f66b79e-a786-4ec2-8c0d-8c420ff56578",
       "name": "?Player 2?",
       "dimensionId": 0,
       "position": [
-        1488.9489,
-        73.62001,
-        -265.6138
+        1806.0983,
+        65.62001,
+        -177.21974
       ],
       "color": "DarkRed",
-      "visible": true
+      "visible": false
     },
     {
       "uuid": "461c568c-cc24-4fbf-9748-6c501d2dc9f2",
       "name": "Jenna",
       "dimensionId": 0,
       "position": [
-        1655.5383,
-        69.62001,
-        -208.74182
+        1348.3186,
+        64.62001,
+        -2066.166
       ],
       "color": "DeepPink",
-      "visible": true
+      "visible": false
     },
     {
       "uuid": "a62cabac-23ca-49ca-ba0a-3d878af3fc6f",
       "name": "Ben",
       "dimensionId": 0,
       "position": [
-        1391.2571,
-        67.62001,
-        -2017.9384
+        2032.4225,
+        68.62001,
+        358.79764
       ],
       "color": "DarkGreen",
-      "visible": true
+      "visible": false
     },
     {
       "uuid": "ae9269d5-ad28-4092-be4b-a8db6c7b567e",
       "name": "?Player 3?",
       "dimensionId": 0,
       "position": [
-        1679.9906,
+        2016.6128,
         75.62001,
-        -212.70615
+        362.32742
       ],
       "color": "DarkRed",
-      "visible": true
+      "visible": false
     },
     {
       "uuid": "f9ffc0a1-24b0-4065-9b89-f92a8864ecbe",
       "name": "?Player 4?",
       "dimensionId": 0,
       "position": [
-        1669.5641,
+        1633.1432,
         69.62001,
-        -180.77008
+        -234.72551
       ],
       "color": "Gold",
-      "visible": true
+      "visible": false
+    },
+    {
+      "uuid": "8aa3efd2-da99-4c98-a520-02ee901518ca",
+      "name": "Player 8aa3e",
+      "dimensionId": 0,
+      "position": [
+        2010.9664,
+        81.62001,
+        362.7644
+      ],
+      "color": "Gold",
+      "visible": false
+    },
+    {
+      "uuid": "edda5469-7338-4518-b05a-5a797949027b",
+      "name": "Player edda5",
+      "dimensionId": 0,
+      "position": [
+        1644.5,
+        65.62001,
+        4.5
+      ],
+      "color": "DarkRed",
+      "visible": false
     }
   ]
 };
