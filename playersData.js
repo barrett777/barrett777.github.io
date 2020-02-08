5bd758be830232aeb81e0e72b05ae14b16a5b8c3
Ben Barrett <barrett777@gmail.com>
2020-02-08 14:27:52 -0800

    Add new map

---
+++ playersData.js
@@ -0,0 +1,80 @@
+// NOTE: Please only modify player attributes below such as name, color, and visible
+// To hide a player's marker, change their 'visible' variable from 'true' to 'false'
+// This file is automatically updated, and if other parts are changed, it will fail to update
+// Changes will only be read from the /map/playersData.js file - /update/playersData.js will be overwritten, so don't make changes there
+var playersData = {
+  "players": [
+    {
+      "uuid": "12f7b0c1-b82b-4dcf-97af-373c4ec4f751",
+      "name": "Player 12f7b",
+      "dimensionId": 0,
+      "position": [
+        -457.53503,
+        96.62001,
+        395.03943
+      ],
+      "color": "DarkBlue",
+      "visible": true
+    },
+    {
+      "uuid": "1337dbd7-7ed0-4898-b5c0-b910c5baedc6",
+      "name": "Player 1337d",
+      "dimensionId": 0,
+      "position": [
+        -1.0598886,
+        97.62001,
+        64.290634
+      ],
+      "color": "DarkRed",
+      "visible": true
+    },
+    {
+      "uuid": "5bcbb8ed-d53f-4bf6-8348-7062e112c3cf",
+      "name": "Player 5bcbb",
+      "dimensionId": 0,
+      "position": [
+        7.735427,
+        69.62001,
+        25.660276
+      ],
+      "color": "DarkBlue",
+      "visible": true
+    },
+    {
+      "uuid": "8e48f1af-2ea6-4de4-a0a4-3b16cc33d32a",
+      "name": "Player 8e48f",
+      "dimensionId": 0,
+      "position": [
+        6.368447,
+        80.62001,
+        228.69789
+      ],
+      "color": "DarkBlue",
+      "visible": true
+    },
+    {
+      "uuid": "b61f3fd2-26f2-4335-b1ec-05fd75d5b992",
+      "name": "Player b61f3",
+      "dimensionId": 0,
+      "position": [
+        -522.3406,
+        71.62001,
+        344.09122
+      ],
+      "color": "SaddleBrown",
+      "visible": true
+    },
+    {
+      "uuid": "eb64ebf2-0df8-4dc9-b7b0-38ed550f3bf9",
+      "name": "Player eb64e",
+      "dimensionId": 0,
+      "position": [
+        5.6971045,
+        71.62001,
+        24.77556
+      ],
+      "color": "SaddleBrown",
+      "visible": true
+    }
+  ]
+};
